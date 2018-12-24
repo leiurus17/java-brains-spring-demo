9c98be82e746e8882511517d88415b7b1b23ccb3
Marlon Monzon <martianworm17@gmail.com>
2018-12-24 17:44:29 +0800

    Using Constructor Injection

--- src/org/tuwaisu/javabrains/Triangle.java
+++ src/org/tuwaisu/javabrains/Triangle.java
@@ -4,6 +4,14 @@ public class Triangle {
 
     private String type;
 
+    public Triangle() {
+
+    }
+
+    public Triangle(String type) {
+        this.type = type;
+    }
+
     public void draw() {
         System.out.println(getType() + " triangle drawn");
     }

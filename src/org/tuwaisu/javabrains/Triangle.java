83c20f81ec3a1a5d86975772c4a8a20267dbfc28
Marlon Monzon <martianworm17@gmail.com>
2018-12-24 21:11:10 +0800

    Injecting Objects

--- src/org/tuwaisu/javabrains/Triangle.java
+++ src/org/tuwaisu/javabrains/Triangle.java
@@ -2,43 +2,35 @@ package org.tuwaisu.javabrains;
 
 public class Triangle {
 
-    private String type;
-    private int height;
-
-    public Triangle() {
-
-    }
-
-    public Triangle(String type) {
-        this.type = type;
-    }
-
-    public Triangle(int height) {
-        this.height = height;
-    }
-
-    public Triangle(String type, int height) {
-        this.type = type;
-        this.height = height;
-    }
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
 
     public void draw() {
-        System.out.println(getType() + " triangle drawn with height " + getHeight());
+
     }
 
-    public String getType() {
-        return type;
+    public Point getPointA() {
+        return pointA;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
     }
 
-    public int getHeight() {
-        return height;
+    public Point getPointB() {
+        return pointB;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
     }
 }
